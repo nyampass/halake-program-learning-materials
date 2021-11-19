b1b5b95962bae646097b7dcdb810b2c97294314f
Tokusei Noborio <tokusei@nyampass.com>
2021-11-20 07:13:14 +0900

    add sample game

--- p5js_2021.10/index.js
+++ p5js_2021.10/index.js
@@ -10,26 +10,43 @@ function setup() {
 
     const sheet2 = loadSpriteSheet('assets/Pixel Adventure 2/Enemies/Ghost/Idle (44x30).png', 44, 30, 10)
     animation2 = loadAnimation(sheet2)
+
+    setInterval(() => {
+        const sp = createSprite( random(width), random(height), 40, 40)        
+        let type = 1
+        if (random(2) < 1) {
+            sp.addAnimation('default', animation1)
+            type = 1
+        } else {
+            sp.addAnimation('default', animation2)
+            type = 2
+        }
+        sp.velocity.y = random(2) - 1
+        sp.velocity.x = random(2) - 1
+        sp.scale = 4
+
+        sp.onMousePressed = (event) => {
+            sp.remove()
+            if (type == 1)
+                score = score + 100
+            else
+                score = score - 500
+        }
+    }, 1000)
 }
+
+let score = 0
 
 function draw() {
     background(50)
-    // drawSprites()
+
+    fill(255)
+    textSize(40)
+    textAlign(CENTER, CENTER)
+    text("SCORE: " + score, width / 2, height / 2)
+
+    drawSprites()
 }
 
 function mousePressed() {
-    const sp = createSprite( random(width), random(height), 40, 40)
-    
-    if (random(2) < 1) {
-      sp.addAnimation('default', animation1)
-    } else {
-      sp.addAnimation('default', animation2)
-    }
-
-    sp.velocity.y = 2
-    sp.velocity.x = 0
-    sp.position.x = mouseX;
-    sp.position.y = mouseY;
-    // sp.rotation = random(360)
-    sp.scale = 1
 }
