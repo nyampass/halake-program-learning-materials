b2b7dc5190b43927feb8c52e449c4e42d4dc3682
Tokusei Noborio <tokusei@nyampass.com>
2021-12-18 13:39:17 +0900

    fix enemy positions

--- p5js_2021.10/avoid.js
+++ p5js_2021.10/avoid.js
@@ -3,10 +3,12 @@ let enemy
 
 let playerSheet
 let enemySheet
+let enemy2Sheet
 
 function preload() {
     playerSheet = loadSpriteSheet('assets/Pixel Adventure 2/Enemies/Chicken/Idle (32x34).png', 32, 34, 13)
     enemySheet = loadSpriteSheet("assets/Pixel Adventure 2/Enemies/Duck/Idle (36x36).png", 36, 36, 10);
+    enemy2Sheet = loadSpriteSheet("assets/Pixel Adventure 2/Enemies/AngryPig/Idle (36x30).png", 36, 30, 9);
 }
 
 function setup() {
@@ -15,54 +17,90 @@ function setup() {
     
     const playerAnimation = loadAnimation(playerSheet)
     const enemyAnimation = loadAnimation(enemySheet)
+    const enemy2Animation = loadAnimation(enemy2Sheet)
 
     player = createSprite(windowWidth / 2, windowHeight / 2)
     player.addAnimation('default', playerAnimation)
-    player.scale = 4
+    player.scale = 2
 
     enemies = new Group()
     setInterval(() => {
-        // enemy = createSprite(random(width), random(height))
         let x
-        if (random() > 0.5) {
+        let y
+        let r = random()
+        if (r < 0.25) {
+            // 左端から出現
             x = 0
-        } else {
+            y = random(height)
+        } else if (r < 0.5) {
+            // 上端から出現
+            x = random(width)
+            y = 0
+        } else if (r < 0.75) {
+            // 右端から出現
             x = width
+            y = random(height)
+        } else {
+            // 下端から出現
+            x = random(width)
+            y = height
         }
-        let y = random(height)        
         enemy = createSprite(x, y)
-        enemy.addAnimation('default', enemySheet)
-        // enemy.velocity.y = random(4) - 1
-        // enemy.velocity.x = random(4) - 1
-        console.log({p: player.position.x, x, z: (x - player.position.x) * 0.1})
-        enemy.velocity.x = (player.position.x - x) * 0.02
-        enemy.velocity.y = (player.position.y - y) * 0.02
-        enemy.scale = 4
+        enemy.addAnimation('default', enemyAnimation)
+        enemy.velocity.x = (player.position.x - x) * 0.01
+        enemy.velocity.y = (player.position.y - y) * 0.01
+        enemy.scale = 2
         enemies.push(enemy)
     }, 1000)
-}
 
-let isGameOver = false
+    setInterval(() => {
+        let x
+        let y
+        let r = random()
+        if (r < 0.25) {
+            // 左端から出現
+            x = 0
+            y = random(height)
+        } else if (r < 0.5) {
+            // 上端から出現
+            x = random(width)
+            y = 0
+        } else if (r < 0.75) {
+            // 右端から出現
+            x = width
+            y = random(height)
+        } else {
+            // 下端から出現
+            x = random(width)
+            y = height
+        }
+        enemy = createSprite(x, y)
+        enemy.addAnimation('default', enemy2Animation)
+        enemy.velocity.x = (player.position.x - x) * 0.02
+        enemy.velocity.y = (player.position.y - y) * 0.02
+        enemy.scale = 2
+        enemies.push(enemy)
+    }, 1500)}
+
+let isGameover = false
 
 function draw() {
     background(50)
 
-    if (isGameOver) {
+    if (isGameover) {
         fill(255)
         textSize(40)
         textAlign(CENTER, CENTER)
-        text("GAMEOVER", width / 2, height / 2)    
+        text("Game Over", width / 2, height / 2)
     }
+ 
+    player.velocity.x = (mouseX - player.position.x) * 0.05;
+    player.velocity.y = (mouseY - player.position.y) * 0.05;
 
-    if (player) {
-        player.velocity.x = (mouseX - player.position.x) * 0.05;
-        player.velocity.y = (mouseY - player.position.y) * 0.05;
-
-        enemies.overlap(player, () => {
-            player.remove()
-            isGameOver = true
-        })
-    }
+    enemies.overlap(player, () => {
+        // player.remove()
+        // isGameover = true
+    })
 
     drawSprites()
 }
